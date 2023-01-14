c9b4f329eebd150ee54c96eacc7955317e864f22
Lepecin <evansealife@gmail.com>
2023-01-14 20:16:30 +0000

    Modify funcs to incorporate changes to objects

--- funcs.py
+++ funcs.py
@@ -1,78 +1,53 @@
-from operators import transmutator, observator
-from utils import relister, nan_detect
 from memory import MemoryDLM, PrimeMemoryDLM
-
-
-def forward_cycle(
-    memory: "MemoryDLM", prime_memory: "PrimeMemoryDLM", time: "int"
-) -> "MemoryDLM":
-
-    # -- Evolution
-
-    # Pick the filtered state model of the given time
-    filtered_state = memory.filtered_states[time]
-
-    # Create evolved state and smoother
-    evolved_state, smoother = transmutator(
-        model=filtered_state, transition=prime_memory.evolver
-    )
-
-    # Append the smoother to list of smoothers
-    memory.smoothers.append(smoother)
-
-    # Append the evolved states to list of evolved states
-    memory.evolved_states.append(evolved_state)
-
-    # -- Observation Preperation
-
-    # -- Evolved Observation
-
-    # Extract current observation
-    observation = prime_memory.observations[time].reshape((-1, 1))
-
-    # Find
-
-    # Create evolved space and filterer
-    evolved_space, filterer = transmutator(
-        model=evolved_state, transition=prime_memory.observer
-    )
-
-    # Append evolved space to list of evolved spaces
-    memory.evolved_spaces.append(evolved_space)
-
-    # -- Filtration
-
-    # Create filtered state
-    filtered_state = observator(observation=observation, transition=filterer)
-
-    # Append filtered state to list of filtered states
-    memory.filtered_states.append(filtered_state)
-
-    # -- Filtered Observation
-
-    # Create filtered space
-    filtered_space = transmutator(
-        model=filtered_state, transition=prime_memory.observer, model_only=True
-    )
-
-    # Append filtered space to list of filtered spaces
-    memory.filtered_spaces.append(filtered_space)
-
-    return memory
+from objects import JointModel
 
 
 def forward(prime_memory: "PrimeMemoryDLM") -> "MemoryDLM":
 
     # Create memory
     memory = MemoryDLM()
-
     # Append the primordial state to filtered states
     memory.filtered_states.append(prime_memory.primordial_model)
-
     # For all times over the forward period
     for time in range(prime_memory.forward_period):
-
         memory = forward_cycle(memory, prime_memory, time)
+
+    return memory
+
+
+def forward_cycle(memory, prime_memory, time):
+
+    filtered_state = memory.filtered_states[time]
+    observation = prime_memory.observations[time]
+    evolver = prime_memory.evolver
+    observer = prime_memory.observer
+
+    # -- Evolution
+    joint_model = JointModel(filtered_state, evolver)
+    joint_model = joint_model.trans_mutator()
+    evolved_state = joint_model.normal
+    smoother = joint_model.transition
+    memory.smoothers.append(smoother)
+    memory.evolved_states.append(evolved_state)
+
+    # -- Observation Preperation
+    observation = observation.reshape((-1, 1))
+
+    # -- Evolved Observation
+    joint_model = JointModel(evolved_state, observer)
+    joint_model = joint_model.trans_mutator()
+    evolved_space = joint_model.normal
+    filterer = joint_model.transition
+    memory.evolved_spaces.append(evolved_space)
+
+    # -- Filtration
+    filtered_state = filterer.observe(observation)
+    memory.filtered_states.append(filtered_state)
+
+    # -- Filtered Observation
+    joint_model = JointModel(filtered_state, observer)
+    filtered_space = joint_model.norm_mutator()
+    memory.filtered_spaces.append(filtered_space)
 
     return memory
 
@@ -84,42 +59,36 @@ def backward(
 
     # Extract final filtered state
     filtered_state = memory.filtered_states[prime_memory.forward_period]
-
-    # Append last filtered state to list of smoothed states
     memory.smoothed_states.append(filtered_state)
 
     # For all times over the forward period
     for time in range(prime_memory.forward_period):
+        memory = backward_cycle(memory, prime_memory, time)
 
-        # -- Smoothed Observation
-
-        # Extract current smoothed state
-        smoothed_state = memory.smoothed_states[time]
-
-        # Create smoothed space
-        smoothed_space = transmutator(
-            model=smoothed_state, transition=prime_memory.observer, model_only=True
-        )
-
-        # Append smoothed space to list of smoothed spaces
-        memory.smoothed_spaces.append(smoothed_space)
-
-        # -- Smoothing
-
-        # Extract current smoother
-        smoother = memory.smoothers[-1 - time]
-
-        # Create smoothed state
-        smoothed_state = transmutator(
-            model=smoothed_state, transition=smoother, model_only=True
-        )
-
-        # Append smoothed state to list of smoothed states
-        memory.smoothed_states.append(smoothed_state)
+    def relister(l):
+        return [l[-1 - i] for i in range(len(l))]
 
     memory.smoothed_states = relister(memory.smoothed_states)
-
     memory.smoothed_spaces = relister(memory.smoothed_spaces)
+
+    return memory
+
+
+def backward_cycle(memory, prime_memory, time):
+
+    observer = prime_memory.observer
+
+    # -- Smoothed Observation
+    smoothed_state = memory.smoothed_states[time]
+    joint_model = JointModel(smoothed_state, observer)
+    smoothed_space = joint_model.norm_mutator()
+    memory.smoothed_spaces.append(smoothed_space)
+
+    # -- Smoothing
+    smoother = memory.smoothers[-1 - time]
+    joint_model = JointModel(smoothed_state, smoother)
+    smoothed_state = joint_model.norm_mutator()
+    memory.smoothed_states.append(smoothed_state)
 
     return memory
 
@@ -131,32 +100,30 @@ def beyond(
 
     # Extract final filtered state
     filtered_state = memory.filtered_states[prime_memory.forward_period]
-
     memory.predicted_states.append(filtered_state)
-
-    # Extract final filtered space
     filtered_space = memory.filtered_spaces[prime_memory.forward_period - 1]
-
     memory.predicted_spaces.append(filtered_space)
 
     for time in range(prime_memory.beyond_period):
+        memory = beyond_cycle(memory, prime_memory, time)
 
-        # -- Prediction
+    return memory
 
-        predicted_state = memory.predicted_states[time]
 
-        predicted_state = transmutator(
-            model=predicted_state, transition=prime_memory.evolver, model_only=True
-        )
+def beyond_cycle(memory, prime_memory, time):
 
-        memory.predicted_states.append(predicted_state)
+    evolver = prime_memory.evolver
+    observer = prime_memory.observer
 
-        # -- Predicted Observation
+    # -- Prediction
+    predicted_state = memory.predicted_states[time]
+    joint_model = JointModel(predicted_state, evolver)
+    predicted_state = joint_model.norm_mutator()
+    memory.predicted_states.append(predicted_state)
 
-        predicted_space = transmutator(
-            model=predicted_state, transition=prime_memory.observer, model_only=True
-        )
-
-        memory.predicted_spaces.append(predicted_space)
+    # -- Predicted Observation
+    joint_model = JointModel(predicted_state, observer)
+    predicted_space = joint_model.norm_mutator()
+    memory.predicted_spaces.append(predicted_space)
 
     return memory
