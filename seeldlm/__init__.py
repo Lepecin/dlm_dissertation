68605ae6417d959ded4107f4b51ad41eb60f2342
Lepecin <evansealife@gmail.com>
2023-02-18 23:36:26 +0000

    Add components to __init__

--- seeldlm/__init__.py
+++ seeldlm/__init__.py
@@ -1,3 +1,4 @@
 from .memory import PrimeMemoryDLM, ModelContainer
 from .funcs import ModellerDLM
 from .objects import NormalModel, TransitionModel, JointModel, InvWishartModel
+from .components import ComponentFactory, model_compiler
