8b8893d4f15b38532c82a0edb781f04aaef4b3bd
Lepecin <evansealife@gmail.com>
2023-02-18 19:19:02 +0000

    Remove updater from init

--- seeldlm/__init__.py
+++ seeldlm/__init__.py
@@ -1,4 +1,3 @@
 from .memory import PrimeMemoryDLM, ModelContainer
-from .updater import UpdaterDLM
 from .funcs import ModellerDLM
 from .objects import NormalModel, TransitionModel, JointModel, InvWishartModel
