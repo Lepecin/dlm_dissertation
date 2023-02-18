dded233e08a16eee56135fb45913bfa0e06bf671
Lepecin <evansealife@gmail.com>
2023-02-18 15:27:14 +0000

    Fix bug

--- memory.py
+++ memory.py
@@ -6,14 +6,41 @@ from objects import NormalModel, TransitionModel, JointModel, InvWishartModel
 class ModelContainer:
     def __init__(self, start: "int", end: "int"):
 
-        self.container: "List[Any]" = (start - end + 1) * [None]
+        self.container: "List[Any]" = max(1, end - start + 1) * [None]
         self.start = start
 
     def get_from_time(self, time: "int") -> "Any":
         return self.container[time - self.start]
 
-    def set_at_time(self, object: "Any", time: "int"):
+    def set_at_time(self, time: "int", object: "Any"):
         self.container[time - self.start] = object
+
+
+class PrimeMemoryDLM:
+    def __init__(
+        self,
+        observed_period: "int",
+        predicted_period: "int",
+        observations: "ndarray",
+        primordial_model: "NormalModel",
+        primordial_error: "InvWishartModel",
+        evolvers: "ModelContainer",
+        observers: "ModelContainer",
+    ):
+
+        self.S = observed_period
+        self.P = predicted_period
+
+        # -- Data
+        self.observations = observations
+
+        # -- Models
+        self.primordial_model = primordial_model
+        self.primordial_error = primordial_error
+
+        # -- Transitions
+        self.evolvers = evolvers
+        self.observers = observers
 
 
 class MemoryDLM:
@@ -58,8 +85,8 @@ class UpdaterDLM(MemoryDLM):
         smoother = joint_model.give_transition()
         evolved_state = joint_model.give_normal()
 
-        self.smoothers.set_at_time(smoother, time + 1)
-        self.evolved_states.set_at_time(evolved_state, time + 1)
+        self.smoothers.set_at_time(time + 1, smoother)
+        self.evolved_states.set_at_time(time + 1, evolved_state)
 
     def observe_evolved(self, time: "int", observer: "TransitionModel"):
 
@@ -70,94 +97,63 @@ class UpdaterDLM(MemoryDLM):
         filterer = joint_model.give_transition()
         evolved_space = joint_model.give_normal()
 
-        self.filterers.set_at_time(filterer, time + 1)
-        self.evolved_spaces.set_at_time(evolved_space, time + 1)
+        self.filterers.set_at_time(time + 1, filterer)
+        self.evolved_spaces.set_at_time(time + 1, evolved_space)
 
     def filter(self, time: "int", observation: "ndarray"):
 
-        evolved_space: "NormalModel" = self.evolved_spaces.get_from_time(
-            time + 1
-        )  # time + 1
-        filterer: "TransitionModel" = self.filterers.get_from_time(time + 1)  # time + 1
-        error: "InvWishartModel" = self.wisharts.get_from_time(time)  # time
+        evolved_space: "NormalModel" = self.evolved_spaces.get_from_time(time + 1)
+        filterer: "TransitionModel" = self.filterers.get_from_time(time + 1)
+        error: "InvWishartModel" = self.wisharts.get_from_time(time)
 
         filtered_state = filterer.observe(observation)
         error = evolved_space.update_wishart(error, observation)
 
-        self.filtered_states.set_at_time(filtered_state, time + 1)
-        self.wisharts.set_at_time(error, time + 1)
+        self.filtered_states.set_at_time(time + 1, filtered_state)
+        self.wisharts.set_at_time(time + 1, error)
 
     def observe_filtered(self, time: "int", observer: "TransitionModel"):
 
-        filtered_state = self.filtered_states.get_from_time(time + 1)  # time + 1
+        filtered_state = self.filtered_states.get_from_time(time + 1)
 
         joint_model = JointModel(filtered_state, observer)
         filtered_space = joint_model.mutate_normal()
 
-        self.filtered_spaces.set_at_time(filtered_space, time + 1)
+        self.filtered_spaces.set_at_time(time + 1, filtered_space)
 
     def smoothen(self, time: "int"):
 
-        smoother = self.smoothers.get_from_time(self.S - time)  # s - time
-        smoothed_state = self.smoothed_states.get_from_time(self.S - time)  # s - time
+        smoother = self.smoothers.get_from_time(self.S - time)
+        smoothed_state = self.smoothed_states.get_from_time(self.S - time)
 
         joint_model = JointModel(smoothed_state, smoother)
         smoothed_state = joint_model.mutate_normal()
 
-        self.smoothed_states.set_at_time(smoothed_state, self.S - time - 1)
+        self.smoothed_states.set_at_time(self.S - time - 1, smoothed_state)
 
     def observe_smoothed(self, time: "int", observer: "TransitionModel"):
 
-        smoothed_state = self.smoothed_states.get_from_time(self.S - time)  # s - time
+        smoothed_state = self.smoothed_states.get_from_time(self.S - time)
 
         joint_model = JointModel(smoothed_state, observer)
         smoothed_space = joint_model.mutate_normal()
 
-        self.smoothed_spaces.set_at_time(smoothed_space, self.S - time)
+        self.smoothed_spaces.set_at_time(self.S - time, smoothed_space)
 
     def predict(self, time: "int", evolver: "TransitionModel"):
 
-        predicted_state = self.predicted_states.get_from_time(self.S + time)  # s + time
+        predicted_state = self.predicted_states.get_from_time(self.S + time)
 
         joint_model = JointModel(predicted_state, evolver)
         predicted_state = joint_model.mutate_normal()
 
-        self.predicted_states.set_at_time(predicted_state, self.S + time + 1)
+        self.predicted_states.set_at_time(self.S + time + 1, predicted_state)
 
     def observe_predicted(self, time: "int", observer: "TransitionModel"):
 
-        predicted_state = self.predicted_states.get_from_time(
-            self.S + time + 1
-        )  # s + time + 1
+        predicted_state = self.predicted_states.get_from_time(self.S + time + 1)
 
         joint_model = JointModel(predicted_state, observer)
         predicted_space = joint_model.mutate_normal()
 
-        self.predicted_spaces.set_at_time(self.S + time + 1)
-
-
-class PrimeMemoryDLM:
-    def __init__(
-        self,
-        observed_period: "int",
-        predicted_period: "int",
-        observations: "ndarray",
-        primordial_model: "NormalModel",
-        primordial_error: "InvWishartModel",
-        evolvers: "ModelContainer",
-        observers: "ModelContainer",
-    ):
-
-        self.S = observed_period
-        self.P = predicted_period
-
-        # -- Data
-        self.observations = observations
-
-        # -- Models
-        self.primordial_model = primordial_model
-        self.primordial_error = primordial_error
-
-        # -- Transitions
-        self.evolvers = evolvers
-        self.observers = observers
+        self.predicted_spaces.set_at_time(self.S + time + 1, predicted_space)
