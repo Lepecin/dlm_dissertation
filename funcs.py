a01d74a55588470e1855a2c99abe09ef63b0d958
Lepecin <evansealife@gmail.com>
2023-02-18 19:06:26 +0000

    Implement DLM modeller class

--- funcs.py
+++ funcs.py
@@ -2,76 +2,73 @@ from memory import PrimeMemoryDLM
 from updater import UpdaterDLM
 
 
-def forward(prime_memory: "PrimeMemoryDLM", memory: "UpdaterDLM") -> "UpdaterDLM":
+class ModellerDLM:
+    def __init__(self, prime_memory: "PrimeMemoryDLM", updater: "UpdaterDLM"):
+        self.prime_memory = prime_memory
+        self.memory = updater
 
-    observed_period = prime_memory.S
-    primordial_model = prime_memory.primordial_model
-    primordial_error = prime_memory.primordial_error
-    evolvers = prime_memory.evolvers
-    observers = prime_memory.observers
-    observations = prime_memory.observations
+    def forward(self):
 
-    memory.filtered_states.set_at_time(0, primordial_model)
-    memory.wisharts.set_at_time(0, primordial_error)
+        observed_period = self.prime_memory.S
+        primordial_model = self.prime_memory.primordial_model
+        primordial_error = self.prime_memory.primordial_error
+        evolvers = self.prime_memory.evolvers
+        observers = self.prime_memory.observers
+        observations = self.prime_memory.observations
 
-    for time in range(observed_period):
+        self.memory.filtered_states.set_at_time(0, primordial_model)
+        self.memory.wisharts.set_at_time(0, primordial_error)
 
-        observation = observations[time]
-        observation = observation.reshape((-1, 1))
-        evolver = evolvers.get_from_time(0)
-        observer = observers.get_from_time(0)
+        for time in range(observed_period):
 
-        memory.evolve(time, evolver)
-        memory.observe_evolved(time, observer)
-        memory.filter(time, observation)
-        memory.observe_filtered(time, observer)
+            observation = observations[time]
+            observation = observation.reshape((-1, 1))
+            evolver = evolvers.get_from_time(0)
+            observer = observers.get_from_time(0)
 
-    return memory
+            self.memory.evolve(time, evolver)
+            self.memory.observe_evolved(time, observer)
+            self.memory.filter(time, observation)
+            self.memory.observe_filtered(time, observer)
 
+    def backward(self):
 
-def backward(
-    prime_memory: "PrimeMemoryDLM",
-    memory: "UpdaterDLM",
-) -> "UpdaterDLM":
+        observed_period = self.prime_memory.S
+        observers = self.prime_memory.observers
 
-    observed_period = prime_memory.S
-    observers = prime_memory.observers
+        filtered_state = self.memory.filtered_states.get_from_time(observed_period)
+        self.memory.smoothed_states.set_at_time(observed_period, filtered_state)
 
-    filtered_state = memory.filtered_states.get_from_time(observed_period)
-    memory.smoothed_states.set_at_time(observed_period, filtered_state)
+        for time in range(observed_period):
 
-    for time in range(observed_period):
+            observer = observers.get_from_time(0)
 
-        observer = observers.get_from_time(0)
+            self.memory.observe_smoothed(time, observer)
+            self.memory.smoothen(time)
 
-        memory.observe_smoothed(time, observer)
-        memory.smoothen(time)
+    def beyond(self):
 
-    return memory
+        observed_period = self.prime_memory.S
+        predicted_period = self.prime_memory.P
+        evolvers = self.prime_memory.evolvers
+        observers = self.prime_memory.observers
 
+        filtered_state = self.memory.filtered_states.get_from_time(observed_period)
+        self.memory.predicted_states.set_at_time(observed_period, filtered_state)
 
-def beyond(
-    prime_memory: "PrimeMemoryDLM",
-    memory: "UpdaterDLM",
-) -> "UpdaterDLM":
+        filtered_space = self.memory.filtered_spaces.get_from_time(observed_period)
+        self.memory.predicted_spaces.set_at_time(observed_period, filtered_space)
 
-    observed_period = prime_memory.S
-    predicted_period = prime_memory.P
-    evolvers = prime_memory.evolvers
-    observers = prime_memory.observers
+        for time in range(predicted_period):
 
-    filtered_state = memory.filtered_states.get_from_time(observed_period)
-    memory.predicted_states.set_at_time(observed_period, filtered_state)
+            evolver = evolvers.get_from_time(0)
+            observer = observers.get_from_time(0)
 
-    filtered_space = memory.filtered_spaces.get_from_time(observed_period)
-    memory.predicted_spaces.set_at_time(observed_period, filtered_space)
+            self.memory.predict(time, evolver)
+            self.memory.observe_predicted(time, observer)
 
-    for time in range(predicted_period):
+    def get_memory(self):
+        return self.memory
 
-        evolver = evolvers.get_from_time(0)
-        observer = observers.get_from_time(0)
-
-        memory.predict(time, evolver)
-        memory.observe_predicted(time, observer)
-
-    return memory
+    def get_prime_memory(self):
+        return self.prime_memory
