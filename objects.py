057a2d21f0e5740810a944155417d3a4818ba9fc
Lepecin <evansealife@gmail.com>
2023-02-18 10:10:28 +0000

    Update objects script to avoid dataclass import

--- objects.py
+++ objects.py
@@ -1,20 +1,17 @@
 import numpy
-from numpy import ndarray
-from dataclasses import dataclass, field
 from typing import Optional
 
 
-def symmetrise(array: "ndarray"):
+def symmetrise(array: "numpy.ndarray") -> "numpy.ndarray":
 
     return (array + array.T) / 2
 
 
-@dataclass
 class NormalModel:
-
-    mean: "ndarray"
-    covariance: "ndarray"
-    inv_covariance: "Optional[ndarray]" = field(default=None, init=False)
+    def __init__(self, mean: "numpy.ndarray", covariance: "numpy.ndarray"):
+        self.mean = mean
+        self.covariance = covariance
+        self.inv_covariance: "Optional[numpy.ndarray]" = None
 
     def invert_covariance(self):
 
@@ -23,42 +20,45 @@ class NormalModel:
 
         return self.inv_covariance
 
-    def update_wishart(self, wishart: "InvWishart", observation: "ndarray"):
+    def update_wishart(self, wishart: "InvWishartModel", observation: "numpy.ndarray"):
 
         error = self.mean - observation  # (P, N)
         inv_covariance = self.invert_covariance()  # (P, P)
-        scale = wishart.scale + (error.T).dot(inv_covariance).dot(error)  # (N, N)
 
+        scale = wishart.scale + (error.T).dot(inv_covariance).dot(error)  # (N, N)
         shape = wishart.shape + self.mean.shape[0]  # int
 
-        return InvWishart(scale, shape)
+        return InvWishartModel(scale, shape)
 
 
-@dataclass
 class TransitionModel:
+    def __init__(
+        self,
+        bias: "numpy.ndarray",
+        weights: "numpy.ndarray",
+        covariance: "numpy.ndarray",
+    ):
 
-    bias: "ndarray"
-    weights: "ndarray"
-    covariance: "ndarray"
+        self.bias = bias
+        self.weights = weights
+        self.covariance = covariance
 
-    def observe(self, observation: "ndarray"):
+    def observe(self, observation: "numpy.ndarray"):
 
-        # Extract models of transition
         B = self.bias
         A = self.weights
         V = self.covariance
 
-        # Create new mean from observation
         mean = B + A.dot(observation)
 
         return NormalModel(mean, V)
 
 
-@dataclass
 class JointModel:
+    def __init__(self, normal: "NormalModel", transition: "TransitionModel"):
 
-    normal: "NormalModel"
-    transition: "TransitionModel"
+        self.normal = normal
+        self.transition = transition
 
     def give_normal(self):
         return self.normal
@@ -74,9 +74,8 @@ class JointModel:
         A = self.transition.weights
         V = self.transition.covariance
 
-        # Create parameters of new model
-        mean: "ndarray" = B + A.dot(M)
-        covariance: "ndarray" = V + A.dot(S).dot(A.T)
+        mean: "numpy.ndarray" = B + A.dot(M)
+        covariance: "numpy.ndarray" = V + A.dot(S).dot(A.T)
 
         return NormalModel(mean, covariance)
 
@@ -88,17 +87,17 @@ class JointModel:
         A = self.transition.weights
 
         inv_covariance = normal.invert_covariance()
-        weights: "ndarray" = S.dot(A.T).dot(inv_covariance)
-        bias: "ndarray" = M - weights.dot(normal.mean)
-        variation: "ndarray" = S - weights.dot(normal.covariance).dot(weights.T)
+        weights: "numpy.ndarray" = S.dot(A.T).dot(inv_covariance)
+        bias: "numpy.ndarray" = M - weights.dot(normal.mean)
+        variation: "numpy.ndarray" = S - weights.dot(normal.covariance).dot(weights.T)
 
         transition = TransitionModel(bias, weights, variation)
 
         return JointModel(normal, transition)
 
 
-@dataclass
-class InvWishart:
+class InvWishartModel:
+    def __init__(self, scale: "numpy.ndarray", shape: "int"):
 
-    scale: "ndarray"
-    shape: "int"
+        self.scale = scale
+        self.shape = shape
