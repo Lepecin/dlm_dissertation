a44c548b6c3e3121d75d745e12e45185db847818
Lepecin <evansealife@gmail.com>
2023-02-19 16:32:54 +0000

    Import model compiler

--- seeldlm/__init__.py
+++ seeldlm/__init__.py
@@ -1,4 +1,4 @@
 from .memory import PrimeMemoryDLM, ModelContainer
 from .funcs import ModellerDLM
 from .objects import NormalModel, TransitionModel, JointModel, InvWishartModel
-from .components import ComponentFactory, model_compiler
+from .components import ComponentFactory, ModelCompiler
