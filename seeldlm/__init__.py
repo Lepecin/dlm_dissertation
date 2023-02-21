28a0f72a0f153ff87e85a16935669ff9cfa36123
Lepecin <evansealife@gmail.com>
2023-02-21 16:03:08 +0000

    Change container imports

--- seeldlm/__init__.py
+++ seeldlm/__init__.py
@@ -1,4 +1,5 @@
-from .memory import PrimeMemoryDLM, ModelContainer
+from .memory import PrimeMemoryDLM
 from .funcs import ModellerDLM
 from .objects import NormalModel, TransitionModel, JointModel, InvWishartModel
 from .components import ComponentFactory, ModelCompiler
+from .containers import NormalContainer, TransitionContainer, InvWishartContainer
