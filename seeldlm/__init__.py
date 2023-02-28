afce16e77e99887da0bdd6589bb242ab14de06bb
Lepecin <evansealife@gmail.com>
2023-02-28 22:26:27 +0000

    Add visualiser to init

--- seeldlm/__init__.py
+++ seeldlm/__init__.py
@@ -3,3 +3,4 @@ from .funcs import ModellerDLM
 from .objects import NormalModel, TransitionModel, JointModel, InvWishartModel
 from .components import ComponentFactory, ModelCompiler
 from .containers import NormalContainer, TransitionContainer, InvWishartContainer
+from .visualiser import VisualDLM
